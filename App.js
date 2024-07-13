83a60f8db5ab0475ed27bcc5e7c7cea86061fd48
Churima <joaocarlosrm2004@gmail.com>
2024-07-13 09:48:08 -0300

    Ajuste

--- App.js
+++ App.js
@@ -4,9 +4,22 @@ import { PaperProvider } from "react-native-paper";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NewTask from "./screens/NewTask";
+import { initializeApp } from "firebase/app";
 
 export default function App() {
   const Stack = createStackNavigator();   
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyDaTXE9dQEciwCOZb_Y6lwKvQGbrNMf-mQ",
+    authDomain: "apptodolist-f166d.firebaseapp.com",
+    projectId: "apptodolist-f166d",
+    storageBucket: "apptodolist-f166d.appspot.com",
+    messagingSenderId: "895582634805",
+    appId: "1:895582634805:web:5ddcaf76577faa65aac27c"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
   return (
     <PaperProvider>
       <NavigationContainer>
